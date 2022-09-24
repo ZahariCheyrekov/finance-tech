f191856404acf876ccb10375eb474d65ff2ec1ae
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-24 13:15:07 +0300

    Create partners component

---
+++ finance-tech/src/components/Partners.js
@@ -0,0 +1,17 @@
+import partners from '../assets/partners.png';
+
+import './Partners.scss';
+
+const Partners = () => {
+    return (
+        <section className="financetech__partners">
+            <img
+                src={partners}
+                alt="FinanceTech Partners"
+                className="partners"
+            />
+        </section>
+    );
+}
+
+export default Partners;
