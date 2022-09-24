dc9a5aec0c9ce4ceb910bc89d332edad918bc352
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-24 17:07:51 +0300

    Add shadow spans

--- finance-tech/src/components/Home/Home.js
+++ finance-tech/src/components/Home/Home.js
@@ -7,9 +7,11 @@ import './Home.scss';
 const Home = () => {
     return (
         <div className="div__wrapper">
+            <span className="shadow__bottom"></span>
             <Navbar />
             <Content />
             <Partners />
+            <span className="shadow__top"></span>
         </div>
     );
 }
