60e8498a4eb26cb28822a3311b47a910bbaafb7c
Zahari Cheyrekov <zahari.cheyreckov999@gmail.com>
2022-09-24 19:08:07 +0300

    Fix app file

--- finance-tech/src/App.js
+++ finance-tech/src/App.js
@@ -1,14 +1,10 @@
-import { Route, Routes } from 'react-router-dom';
-
 import Home from './components/Home/Home';
 
 import './App.scss';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-    </Routes>
+    <Home />
   );
 }
 
